fc9069a82bd08c4f631565793af18403797f7c46
airhead-angel <154863938+airhead-angel@users.noreply.github.com>
2023-12-26 01:53:43 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(1500, 2250);
+}
+
+function draw() {
+  background("#003366");
+}
+function setup() {
+  createCanvas(1500, 2250);
+  img = loadImage('https://media.discordapp.net/attachments/595047755869847570/1189089572613783552/f2.gif?ex=659ce4f6&is=658a6ff6&hm=a7def3d31a2d6f6e8506675281d6d6b2712dd3362e753eef308e1e354d72a7ce&=&width=441&height=662'); // Load the image
+}
+
+function draw() {
+image(img, 0, 0);
+}
